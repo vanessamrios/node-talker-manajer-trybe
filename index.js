908ebb46f34039cf98721007d8c43877d55ec7d2
Vanessa <vanessamrios@gmail.com>
2022-02-16 16:08:51 +0100

    Cria o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,18 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+function handleGetTalker(_req, res) {
+  const talkersJason = fs.readFileSync('talker.json', 'utf-8');
+  const talkers = JSON.parse(talkersJason);
+
+  if (!talkers.length) {
+    res.status(200).send(talkers);
+  }
+  return res.status(200).send(talkers);
+}
+
+app.get('/talker', handleGetTalker);
 
 app.listen(PORT, () => {
   console.log('Online');
