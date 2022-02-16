38abade9808ac72fd8da05af2fb4d52393ffe75e
Vanessa <vanessamrios@gmail.com>
2022-02-16 21:12:27 +0100

    Cria o endpoint POST /login

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// req 01
+
 function handleGetAllTalkers(_req, res) {
   const talkersJason = fs.readFileSync('talker.json', 'utf-8');
   const talkers = JSON.parse(talkersJason);
@@ -24,6 +26,8 @@ function handleGetAllTalkers(_req, res) {
 }
 
 app.get('/talker', handleGetAllTalkers);
+
+// req 02
 
 function handleGetTalkerById(_req, res) {
   const talkersJason = fs.readFileSync('talker.json', 'utf-8');
@@ -40,6 +44,39 @@ function handleGetTalkerById(_req, res) {
 }
 
 app.get('/talker/:id', handleGetTalkerById);
+
+// req 3
+
+function isEmailValid(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function isPasswordValid(password) {
+  const minPasswordSize = 6;
+  return password.length >= minPasswordSize;
+}
+
+function handleLogin(_req, res) {
+  const { email, password } = _req.body;
+  if (!email) {
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!isEmailValid(email)) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!isPasswordValid(password)) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(200).send({
+    token: 'kdie7h6uo274kf8h',
+  });
+}
+
+app.post('/login', handleLogin);
 
 app.listen(PORT, () => {
   console.log('Online');
